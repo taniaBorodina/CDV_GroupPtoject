788d9a6378f5ca5a6a12b52a07beb4c86381b214
Kamil <kubiak.kamil23@gmail.com>
2023-12-18 21:36:14 +0100

    add weather api

--- scripts.js
+++ scripts.js
@@ -27,3 +27,40 @@ function countDownFun() {
 }
 
 setInterval(countDownFun, 1000);
+
+function getWeather() {
+  const weatherData = () => {
+    fetch("https://danepubliczne.imgw.pl/api/data/synop/id/12330")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Brak odpowiedzi z serwera");
+        }
+        console.log("Dane z servera OK!");
+        return response.json();
+      })
+      .then((data) => {
+        const { stacja, data_pomiaru, suma_opadu, cisnienie, temperatura } =
+          data;
+
+        // Formatowanie daty na 'd.m.Y'
+        const dataSformatowana = new Date(data_pomiaru);
+        const dzien = dataSformatowana.getDate().toString().padStart(2, "0");
+        const miesiac = (dataSformatowana.getMonth() + 1)
+          .toString()
+          .padStart(2, "0");
+        const rok = dataSformatowana.getFullYear();
+        // przypisywanie danych do span
+        document.getElementById("city").textContent = `${stacja}`;
+        document.getElementById("date").textContent = `${dzien}.${miesiac}.${rok} r.`;
+        document.getElementById("rainfall").textContent = `Opady: ${suma_opadu} mm/h`;
+        document.getElementById("pressure").textContent = `Ciśnienie: ${cisnienie} hPa`;
+        document.getElementById("temperature").innerHTML = `Temp. ${temperatura} &#176;C`;
+      })
+      .catch((error) => {
+        console.error("Błąd pobierania danych:", error);
+      });
+  };
+  weatherData();
+  setInterval(weatherData, 60000);
+}
+getWeather();
